59a262d4d31ffa5c0166e3f6edb53b97741ee77a
hstratton22 <hstrat@byui.edu>
2020-01-08 11:54:54 -0500

    Update

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,8 +1,8 @@
 
             //const options={minute: "numeric", hour: "numeric", day: "numeric", month: "long", }
             const options= {year:"numeric"};
-           // document.getElementById('currentdate').textContent=new Date().toLocaleDateString("en-US",);
+          
             document.getElementById('currentdate').textContent=new Date().toLocaleDateString("en-US", options);
-           const modoptions ={ second: "numeric", minute: "numeric", hour: "numeric", day: "numeric", month:"numeric", year: "numeric"};
+          // const modoptions ={ second: "numeric", minute: "numeric", hour: "numeric", day: "numeric", month:"numeric", year: "numeric"};
             document.getElementById('lastModified').textContent=new Date(document.lastModified);
   
