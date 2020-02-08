c66b390a61e08337c4ebc387f35115cbf7a13c64
hstratton22 <hstrat@byui.edu>
2020-02-08 16:28:04 -0500

    update js windchill

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,12 +1,14 @@
 
 function windChill() {
+    
     let t = parseFloat(document.getElementById("temp").innerHTML);
     let s = parseFloat(document.getElementById("windspeed").innerHTML);
-    console.log(t);
+    //console.log(s);
+    let f;
     if (t<=50 && s>=3.0) {
     
-    let f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
-    f= f.toFixed(2);
+    f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
+    f= f.toFixed(0);
     }
     else {f= "N/A";}
    
