3d411a96debd0b3152af8a9bccd8f8764ce6b121
hstratton22 <hstrat@byui.edu>
2020-02-13 08:05:18 -0500

    js without onload

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,16 +1,32 @@
 
+
+    
+    let t = parseFloat(document.getElementById("temp").textContent);
+    let s = parseFloat(document.getElementById("windspeed").textContent);
+    
+    let f;
+    if (t<=50 && s>=3.0) {
+    
+    f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
+    f= f.toFixed(0);
+    }
+    else {f= "N/A";}
+   
+    document.getElementById("windchill").textContent = f;
+
+/*//works with <body onload="windChill()">
 function windChill() {
     
-    let t = parseFloat(document.getElementById("temp").innerHTML);
-    let s = parseFloat(document.getElementById("windspeed").innerHTML);
+    let t = parseFloat(document.getElementById("temp").textContent);
+    let s = parseFloat(document.getElementById("windspeed").textContent);
     //console.log(s);
     let f;
     if (t<=50 && s>=3.0) {
     
     f = 35.74 + 0.6215*t - 35.75*s**0.16 + 0.4275*t*s**0.16;
-    f= f.toFixed(0)+" &#8457;";
+    f= f.toFixed(0)+ "&#8457;";
     }
     else {f= "N/A";}
    
-    document.getElementById("windchill").innerHTML = f;
-}
+    document.getElementById("windchill").textContent = f;
+}*/
