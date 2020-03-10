4066556f420b79a4f273dc970aa21603fdb7f73b
hstratton22 <hstrat@byui.edu>
2020-03-10 07:04:17 -0400

    comment console lesson10

--- lesson10/js/windchill.js
+++ lesson10/js/windchill.js
@@ -14,7 +14,7 @@ const apiURL="https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    console.log(jsObject);
+    //console.log(jsObject);
     document.getElementById('current').textContent = jsObject.list[0].weather[0].main;
     document.getElementById('temp').textContent = jsObject.list[0].main.temp_max.toFixed(0);
     document.getElementById('windspeed').textContent = jsObject.list[0].wind.speed.toFixed(0);
