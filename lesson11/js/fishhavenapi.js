6b4d136c82712a1b55e69507e49758e272f4a0ae
hstratton22 <hstrat@byui.edu>
2020-03-12 18:33:51 -0400

    towns js and css for events

--- lesson11/js/fishhavenapi.js
+++ lesson11/js/fishhavenapi.js
@@ -55,3 +55,38 @@ fetch(apiURL)
    
      
     });
+    /*==================================================*/
+    const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+ 
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  
+  .then(function (jsonObject) {
+    //console.table(jsonObject);        // temporary checking for valid response and data parsing 
+    const towns = jsonObject['towns'];
+   for (let i = 0; i < towns.length; i++ ) {
+       if ( towns[i].name =="Fish Haven" ) {
+    let section = document.createElement('section');
+    let h4 = document.createElement('h4');
+    let x;
+    for (let x=0; x < towns[i].events.length; x++) {
+      h4.innerHTML += towns[i].events[x] + "<br>" ;
+      section.appendChild(h4);
+      
+
+
+    }
+ 
+
+   /* h4.textContent = towns[i].events[0];
+    h4.textContent = towns[i].events[1];
+    h4.textContent = towns[i].events[2];*/
+   // h4.textContent = towns[i].events;
+//section.appendChild(h4);
+
+document.querySelector('div.events').appendChild(section);
+}
+}
+});
