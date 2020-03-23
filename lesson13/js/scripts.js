7da65055b2ddf236dd4c2a87bcf11a0ad3d2c03c
hstratton22 <hstrat@byui.edu>
2020-03-23 16:11:57 -0400

    json

--- lesson13/js/scripts.js
+++ lesson13/js/scripts.js
@@ -5,16 +5,35 @@ function menu() {
    
     document.getElementById("topNav").classList.toggle("hide");
 }
-/*---------------------weather-----*/
-const apiURL="https://api.openweathermap.org/data/2.5/forecast?id=4092267&units=imperial&APPID=e7e99f72ef4dc80d87dfcaa6b6420751";
-fetch(apiURL)
-.then((response) => response.json())
-.then((jsObject)=>{
-    //console.log(jsObject);
-    document.getElementById('current').textContent=jsObject.list[0].weather[0].main;
-    document.getElementById('high').textContent = jsObject.list[0].main.temp_max.toFixed(0);
-    document.getElementById('low').textContent = jsObject.list[0].main.temp_min.toFixed(0);
 
-}
-)
-/*=====================*/
+/*========guides=============*/
+const guidebios ="bios.json";
+fetch(guidebios)
+.then(function(response){
+    return response.json();
+
+})
+.then(function(jsonObject) {
+    console.table(jsonObject);
+    const bios = jsonObject['bios'];
+    for (let i=0; i <bios.length; i++) {
+        let profile = document.createElement('section');
+        let ind = document.createElement('div');
+        let h3 =document.createElement('h3');
+        let h4= document.createElement('h4');
+        let image=document.createElement('img');
+
+        h3.textContent =bios[i].firstName;
+        h3.textContent=bios[i].lastName;
+        h4.textContent=bios[i].certLevel;
+        h4.textContent=bios[i].email;
+        h4.textContent=bios[i].bio;
+        image.setAttribute('src', "images/" + bios[i].profile);
+        image.setAttribute('alt', bios[i].lastName + "photo");
+
+        h3.appendChild(ind);
+        h4.appendChild(ind);
+        document.querySelector('div.profiles').appendChild(profile);
+    }
+
+})
---
+++ lesson13/js/weather.js
@@ -0,0 +1,12 @@
+/*---------------------weather-----*/
+const apiURL="https://api.openweathermap.org/data/2.5/forecast?id=4092267&units=imperial&APPID=e7e99f72ef4dc80d87dfcaa6b6420751";
+fetch(apiURL)
+.then((response) => response.json())
+.then((jsObject)=>{
+    //console.log(jsObject);
+    document.getElementById('current').textContentL=jsObject.list[0].weather[0].main;
+    document.getElementById('high').textContent = jsObject.list[0].main.temp_max.toFixed(0);
+    document.getElementById('low').textContent = jsObject.list[0].main.temp_min.toFixed(0);
+
+}
+)
