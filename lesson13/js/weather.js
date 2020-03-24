510241f5d9d1a4bcb783350b183cd185975f1117
hstratton22 <hstrat@byui.edu>
2020-03-24 08:24:48 -0400

    forecast

--- lesson13/js/weather.js
+++ lesson13/js/weather.js
@@ -3,10 +3,10 @@ const apiURL="https://api.openweathermap.org/data/2.5/forecast?id=4092267&units=
 fetch(apiURL)
 .then((response) => response.json())
 .then((jsObject)=>{
-    //console.log(jsObject);
-    document.getElementById('current').textContentL=jsObject.list[0].weather[0].main;
+    console.log(jsObject);
+    document.getElementById('current').textContent=jsObject.list[0].weather[0].main;
     document.getElementById('high').textContent = jsObject.list[0].main.temp_max.toFixed(0);
-    document.getElementById('low').textContent = jsObject.list[0].main.temp_min.toFixed(0);
+    document.getElementById('humidity').textContent = jsObject.list[0].main.humidity.toFixed(0);
 
 }
 )
