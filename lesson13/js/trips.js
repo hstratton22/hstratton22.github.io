c86fee493696dd97419b9cbe24849c8d6a986d09
hstratton22 <hstrat@byui.edu>
2020-03-26 10:59:03 -0400

    adventures

--- lesson13/js/trips.js
+++ lesson13/js/trips.js
@@ -10,19 +10,19 @@ fetch(trips)
     const desc =jsonObject['trips'];
     for (let i=0; i < desc.length; i++) {
         let location =document.createElement('h3');
-        let cost= document.createElement('h3');
-        let skill=document.createElement('h3');
-        let services=document.createElement('h3');
-        let len=document.createElement('h3');
+        let cost= document.createElement('p');
+        let skill=document.createElement('p');
+        let services=document.createElement('p');
+        let len=document.createElement('p');
         let text = document.createElement('div');
         let image = document.createElement('img');
         let card = document.createElement('section');
 
         location.textContent=desc[i].location;
-        len.textContent=desc[i].length;
-        skill.textContent=desc[i].skill;
-        cost.textContent=desc[i].cost;
-        services.textContent=desc[i].services;
+        len.textContent="Length: " + desc[i].length;
+        skill.textContent="Skill level: " + desc[i].skill;
+        cost.textContent=desc[i].cost + " per person";
+        services.textContent="Details: " + desc[i].services;
         image.setAttribute('src', "images/" + desc[i].photo);
         image.setAttribute('alt', desc[i].photo);
 
